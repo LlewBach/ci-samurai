94b1a7a4d6514fe4fc69202d7614e9b0e039bee0
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-02-07 21:01:38 +0000

    Test Layer and Background class draw methods

--- scripts/background/background.test.js
+++ scripts/background/background.test.js
@@ -5,10 +5,12 @@ describe('Layer class', () => {
   let game;
   let layer;
   let mockImage = {};
+  let mockContext;
 
   beforeEach(() => {
     game = new Game(800, 600);
     layer = new Layer(game, 3199, 943, mockImage, 1);
+    mockContext = { drawImage: jest.fn() };
   });
 
   test('should create an instance of Layer', () => {
@@ -35,22 +37,29 @@ describe('Layer class', () => {
     expect(layer.y).toBe(0);
   });
 
-  test('should enforce the left boundary', () => {
+  test('.update should enforce the left boundary', () => {
     layer.x = 50;
     layer.update();
     expect(layer.x).toBe(0);
   });
 
-  test('should reset layer', () => {
+  test('.update should reset layer', () => {
     layer.x = -3200;
     layer.update();
     expect(layer.x).toBe(0);
   });
 
-  test('should subtract correct amound from x', () => {
+  test('.update should subtract correct amound from x', () => {
     game.speed = 3;
     layer.update();
     expect(layer.x).toBe(0 - (3 * 1));
+  });
+
+  test('.draw should call context.drawImage correctly', () => {
+    layer.draw(mockContext);
+    expect(mockContext.drawImage).toHaveBeenCalledTimes(2);
+    expect(mockContext.drawImage).toHaveBeenCalledWith(mockImage, layer.x, layer.y, layer.width, layer.height);
+    expect(mockContext.drawImage).toHaveBeenCalledWith(mockImage, layer.x + layer.width, layer.y, layer.width, layer.height);
   });
 });
 
@@ -59,6 +68,7 @@ describe('Background class', () => {
   let game;
   let background;
   let mockLayerUpdate;
+  let mockContext;
 
   beforeEach(() => {
     game = new Game(800, 600);
@@ -84,7 +94,7 @@ describe('Background class', () => {
     expect(background).toHaveProperty('backgroundLayers');
   });
 
-  test('update should call update on each layer', () => {
+  test('.update should call update on each layer', () => {
     mockLayerUpdate = jest.fn();
     background.layer1.update = mockLayerUpdate;
     background.layer2.update = mockLayerUpdate;
@@ -92,5 +102,19 @@ describe('Background class', () => {
     background.layer4.update = mockLayerUpdate;
     background.update();
     expect(mockLayerUpdate).toHaveBeenCalledTimes(4);
+  });
+
+  test('.draw method should call draw on each layer with context', () => {
+    mockContext = { drawImage: jest.fn() };
+    background.layer1.draw = jest.fn();
+    background.layer2.draw = jest.fn();
+    background.layer3.draw = jest.fn();
+    background.layer4.draw = jest.fn();
+
+    background.draw(mockContext);
+    expect(background.layer1.draw).toHaveBeenCalledWith(mockContext);
+    expect(background.layer2.draw).toHaveBeenCalledWith(mockContext);
+    expect(background.layer3.draw).toHaveBeenCalledWith(mockContext);
+    expect(background.layer4.draw).toHaveBeenCalledWith(mockContext);
   });
 });
