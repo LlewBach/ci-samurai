e5513f9383baf9d149326d794a15baa34e776c66
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-02-08 20:04:45 +0000

    Test input.js

--- scripts/input/input.test.js
+++ scripts/input/input.test.js
@@ -3,7 +3,43 @@ import { InputHandler } from './input.js';
 describe('InputHandler', () => {
   let inputHandler;
 
-  // beforeEach(() => {
+  beforeEach(() => {
+    // Mocking window.addEventListener to prevent actual event listeners from being added
+    jest.spyOn(window, 'addEventListener');
+    inputHandler = new InputHandler();
+  });
 
-  // });
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  const simulateEvent = (type, key) => {
+    const event = new KeyboardEvent(type, { key });
+    window.dispatchEvent(event);
+  }
+
+  test('add ArrowLeft to keys on keydown', () => {
+    simulateEvent('keydown', 'ArrowLeft');
+    expect(inputHandler.keys).toContain('ArrowLeft');
+  });
+
+  test('removes ArrowLeft from keys on keyup', () => {
+    simulateEvent('keydown', 'ArrowLeft');
+    simulateEvent('keyup', 'ArrowLeft');
+    expect(inputHandler.keys).not.toContain('ArrowLeft');
+  });
+
+  test('does not add duplicates of ArrowLeft', () => {
+    simulateEvent('keydown', 'ArrowLeft');
+    simulateEvent('keydown', 'ArrowLeft');
+    expect(inputHandler.keys.filter(key => key === 'ArrowLeft').length).toBe(1);
+  });
+
+  it('ignores keys other than ArrowLeft and ArrowRight', () => {
+    simulateEvent('keydown', 'p');
+    expect(inputHandler.keys).toEqual([]);
+  });
 });
+
+// Pretty much all of above directly from GPT-4
+
