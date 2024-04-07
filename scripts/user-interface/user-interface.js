c6fffacde8f1e34301a926adf77643b0f8958fb6
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-04-07 10:32:57 +0100

    Capitalize key references

--- scripts/user-interface/user-interface.js
+++ scripts/user-interface/user-interface.js
@@ -94,69 +94,69 @@ export class UI {
     // New game message
     if (this.game.isFreshGame) {
       this.text1 = 'Die well, Coder-san';
-      this.text2 = 'Press spacebar or swipe up to start/pause';
-      this.text3 = 'Press t or swipe left for Training Mode';
+      this.text2 = 'Press Spacebar or swipe up to start/pause';
+      this.text3 = 'Press T or swipe left for Training Mode';
     }
     // Training Mode Messages - Note message differentiation depending on isTouchScreen flag.
     else if (this.game.trainingMode) {
       if (this.game.score === 0) {
         this.text1 = "Jump";
-        if (!this.game.isTouchScreen) this.text2 = "Press i key";
+        if (!this.game.isTouchScreen) this.text2 = "Press I key";
         else this.text2 = "Push joystick up";
         this.text3 = 'Jumping gives you energy!';
       } else if (this.game.score === 1) {
         this.text1 = "Learn to roll";
-        if (!this.game.isTouchScreen) this.text2 = "Press k key";
+        if (!this.game.isTouchScreen) this.text2 = "Jump and press K key";
         else this.text2 = "Push joystick down";
         this.text3 = 'Roll to land safely and escape attacks!';
       } else if (this.game.score === 2) {
         this.text1 = "Run right";
-        if (!this.game.isTouchScreen) this.text2 = "Press l key";
+        if (!this.game.isTouchScreen) this.text2 = "Press L key";
         else this.text2 = "Push joystick right";
         this.text3 = '';
       } else if (this.game.score === 3) {
         this.text1 = "Run left";
-        if (!this.game.isTouchScreen) this.text2 = "Press j key";
+        if (!this.game.isTouchScreen) this.text2 = "Press J key";
         else this.text2 = "Push joystick left";
         this.text3 = '';
       } else if (this.game.score === 4) {
         this.text1 = "Attack1 right";
-        if (!this.game.isTouchScreen) this.text2 = "Press a key";
+        if (!this.game.isTouchScreen) this.text2 = "Press A key";
         else this.text2 = "Press button 1";
         this.text3 = 'Attack1 takes 1 energy';
       } else if (this.game.score === 5) {
         this.text1 = "Attack1 left";
-        if (!this.game.isTouchScreen) this.text2 = "Press shift + a key";
+        if (!this.game.isTouchScreen) this.text2 = "Press Shift + A key";
         else this.text2 = "Hold down joystick and press button 1";
         this.text3 = 'Attack 1 kills one enemy in short range';
       } else if (this.game.score === 6) {
         this.text1 = "Attack2 right";
-        if (!this.game.isTouchScreen) this.text2 = "Press s key";
+        if (!this.game.isTouchScreen) this.text2 = "Press S key";
         else this.text2 = "Press button 2";
         this.text3 = 'Attack2 takes 5 energy';
       } else if (this.game.score === 7) {
         this.text1 = "Attack2 left";
-        if (!this.game.isTouchScreen) this.text2 = "Press shift + s key";
+        if (!this.game.isTouchScreen) this.text2 = "Press Shift + S key";
         else this.text2 = "Hold down joystick and press button 2";
         this.text3 = 'Attack2 kills all enemies in short range';
       } else if (this.game.score === 8) {
         this.text1 = "Attack3 right";
-        if (!this.game.isTouchScreen) this.text2 = "Press d key";
+        if (!this.game.isTouchScreen) this.text2 = "Press D key";
         else this.text2 = "Press button 3";
         this.text3 = 'Attack3 takes 30 energy';
       } else if (this.game.score === 9) {
         this.text1 = "Attack3 left";
-        if (!this.game.isTouchScreen) this.text2 = "Press shift + d key";
+        if (!this.game.isTouchScreen) this.text2 = "Press Shift + D key";
         else this.text2 = "Hold down joystick and press button 3";
         this.text3 = 'Long range and generates +5 health';
       } else if (this.game.score === 10) {
         this.text1 = 'Attack4';
-        if (!this.game.isTouchScreen) this.text2 = 'Press f key';
+        if (!this.game.isTouchScreen) this.text2 = 'Press F key';
         else this.text2 = "Press button 4";
         this.text3 = 'Generates +25 health';
       } else if (this.game.score === 11) {
         this.text1 = 'Your training is complete';
-        if (!this.game.isTouchScreen) this.text2 = 'Press r to go to start screen';
+        if (!this.game.isTouchScreen) this.text2 = 'Press R to go to start screen';
         else this.text2 = "Swipe left to go to start screen";
         this.text3 = '';
       }
@@ -176,8 +176,8 @@ export class UI {
     } else if (!this.game.trainingMode && !this.game.isFreshGame && this.game.isPaused) {
       this.text1 = "Game Paused";
       if (!this.game.isTouchScreen) {
-        this.text2 = "Press spacebar to continue";
-        this.text3 = "Remember r key is restart";
+        this.text2 = "Press Spacebar to continue";
+        this.text3 = "Remember R key is restart";
       }
       else {
         this.text2 = "Swipe up to continue";
@@ -196,7 +196,7 @@ export class UI {
         this.text1 = 'If you can\'t beat them...';
         this.text2 = 'Join them, Coder-san';
       }
-      if (!this.game.isTouchScreen) this.text3 = 'Press r key to restart';
+      if (!this.game.isTouchScreen) this.text3 = 'Press R key to restart';
       else this.text3 = 'Swipe up to restart';
     } else {
       this.text1 = '';
